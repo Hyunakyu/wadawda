70578ecdd248ad7b79f1df3479c80327e3328daa
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 00:27:23 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -6,17 +6,10 @@ exports.run = async (bot, message, args) => {
 		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
         .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
         .setAuthor("Help Command")
-		.addField("Prefix","Gunakan . Saat Menggunakan Command, Saat Ini Tidak Bisa Diganti")
-        .addField("1. -ping", "cuma buat test Latency:3")
-        .addField("2. -play / -p", "Untuk Memutar Musik Contoh -play momoland")
-        .addField("3. -skip", "skip lagu")
-        .addField("4. -stop", "memberhentikan lagu")
-        .addField("5. -np", "Mengetahui lagu yang lagi diplay")
-        .addField("6. -queue / -q", "Melihat List Lagu yang dimasukan")
-        .addField("7. -kick", "Cara -kick@Mention user untuk mengkick, Need Permission Role High")
-        .addField("8. -ban", "Cara -ban@Mention user untuk mengkick, Need Permission Role Admin/Administrator")
-        .addField("9. -serverinfo", "Mengetahui Informasi Server Discord")
-        .addField("10. -botinfo", "Mengetahui Pembuat Bot");
+		.addField("Prefix - ","Change Prefix | -prefix")
+        .addField("1. -Command Music", "gunakan -music untuk melihat command music")
+        .addField("2. -Command Anime", "gunakan -anime untuk melihat command anime")
+        .addField("3. -Other Command", "gunakan -other untuk melihat command other");
 
 		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
 }
