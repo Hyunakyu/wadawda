a7c729ccc1f3294bd3b702d7a140b5372e3e60ad
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-23 22:25:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.on("message", async message => {
 	const db = await dbPromise;
 	db.get(`SELECT * FROM prefixes WHERE guildId = "${message.guild.id}"`).then(row => {
 		let prefix;
-		if (!row) {
+		if (-row) {
 			prefix = config.prefix
 		} else {
 			prefix = row.prefix
@@ -27,16 +27,16 @@ bot.on("message", async message => {
 		let args = message.content.slice(prefix.length).trim().split(" ");
 		let cmd = args.shift().toLowerCase();
 		//mention bot for get the prefix
-		if (msg === `<@${bot.user.id}>` || msg === `<@!${bot.user.id}>` || msg === "!prefix" || msg === `${prefix}prefix`){
+		if (msg === `<@${bot.user.id}>` || msg === `<@-${bot.user.id}>` || msg === "-prefix" || msg === `${prefix}prefix`){
 			db.get(`SELECT * FROM prefixes WHERE guildId - "${message.guild.id}"`).then(row => {
-				if(!row){
+				if(-row){
 					message.channel.send(`**${message.member.user.tag}** my prefix for this server is \`${prefix}\``)
 				}else{
 					message.channel.send(`**${message.member.user.tag}** my prefix for this server is \`${row.prefix}\``)
 				}
 			})
 		}
-		if (!msg.startsWith(prefix)) return;
+		if (-msg.startsWith(prefix)) return;
 	
 		try {
 			let commandFile = require(`./cmds/${cmd}.js`);
