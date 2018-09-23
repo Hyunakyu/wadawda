99ba6a61c2c5f04619b0b27e28e0c107a2b8d269
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 01:28:35 +0700

    Update fun.js

--- cmds/fun.js
+++ cmds/fun.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("<:stuck_out_tongue_closed_eyes:> Fun")
     .setTimestamp()
     .addField("-8ball", "Ask the magical 8ball a question.")
-    .addField("-message.prefix +"avatar`", "Get a member's avatar.")
+    .addField("-avatar", "Get a member's avatar.")
     .addField("-doggo", "Get a random dog image.")
     .addField("-cat", "Get random cat image.")
     .addField("-meme", "Get a random meme.")
