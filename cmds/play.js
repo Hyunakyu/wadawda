e78ef67127f32ec9f23d9e483917d5bacc992461
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 00:10:25 +0700

    Update play.js

--- cmds/play.js
+++ cmds/play.js
@@ -10,8 +10,6 @@ module.exports.run = async (bot, message, args) => {
 var searchString = args.slice(1).join(' ');
 	var url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 	var serverQueue = queue.get(message.guild.id);
-    switch (args[0].toLowerCase()) {
-      case "play": case "p":
     var voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
 		var permissions = voiceChannel.permissionsFor(message.client.user);
@@ -60,7 +58,6 @@ Please provide a value to select one of the search results ranging from 1-10.
 				}
 			}
 			return handleVideo(video, message, voiceChannel);
-		}
 
 async function handleVideo(video, message, voiceChannel, playlist = false) {
 	var serverQueue = queue.get(message.guild.id);
