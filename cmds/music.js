03a4ff1f780eb497a7448f906f64a024a7b0a2c9
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 00:33:34 +0700

    Create music.js

---
+++ cmds/music.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+		let bicon = bot.user.displayAvatarURL; // untuk menampilkan avatar dari bot kalian
+		let botembed = new Discord.RichEmbed()
+		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
+        .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
+        .setAuthor("Music Command")
+        .addField("1. -play / -p", "Untuk Memutar Musik");
+
+		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
+}
+
+exports.help = {
+	name: "help"
+}
