ba41b1cdfa625b46bb4291bb71d2cebc5109b3ca
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-23 22:32:37 +0700

    Update prefix.js

--- cmds/prefix.js
+++ cmds/prefix.js
@@ -4,12 +4,12 @@ const dbPromise = sql.open(`./sql/tutorial.sqLite`, {Promise});
 
 exports.run = async (bot, message, args) => {
     const db = await dbPromise;
-    if (!message.member.hasPermission("MANAGE_GUILD")) return;
-    if (!args[0]) return;
+    if (-message.member.hasPermission("MANAGE_GUILD")) return;
+    if (-args[0]) return;
     db.get(`SELECT * FROM prefixes WHERE guildId = "${message.guild.id}"`).then(row => {
-        if (!row) {
+        if (-row) {
             db.run("INSERT INTO prefixes (guildId, prefix) VALUES (?, ?)", [message.guild.id, args[0]]);
-            message.channel.send(`**${message.member.user.tag}** changed prefix on this server form \`!\` to \`${args[0]}\``);
+            message.channel.send(`**${message.member.user.tag}** changed prefix on this server form \`-\` to \`${args[0]}\``);
 
         }else{
             db.run(`UPDATE prefixes SET prefix = "${args[0]}" WHERE guildId = "${message.guild.id}"`);
