dabb038e98011c3766d3ce38dd4205da2c79ce82
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 16:32:07 +0700

    Update reboot.js

--- cmds/reboot.js
+++ cmds/reboot.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 const config = require("./config.json")
 
 module.exports.run = async (bot, message, args) => { 
-    if(message.author.id !== "264015893434990592" ) return message.channel.send("You cannot use this command because, you are not a developer.")
+    if(message.author.id !== "264015893434990592" ) return message.channel.send(":no_entry_sign: You cannot use this command because, you are not a developer.")
 
   rebootBot(message.channel);
        function rebootBot(channel) {
