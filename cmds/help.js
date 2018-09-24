737fa4a607a8f1068b2802d572bab0783c5c8847
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:03:11 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -7,9 +7,10 @@ exports.run = async (bot, message, args) => {
         .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
         .setAuthor("Help Command")
 		.addField("Prefix - ","Change Prefix | -prefix")
-        .addField("1. -Command Music", "gunakan -music untuk melihat command music")
-        .addField("2. -Command Anime", "gunakan -hanime untuk melihat command anime")
-        .addField("3. -Other Command", "gunakan -other untuk melihat command other");
+        .addField("1. Command Music", "gunakan -music untuk melihat command music")
+	.addField("2. Command Fun", "gunakan -fun untuk melihat command music")
+        .addField("2. Command Anime", "gunakan -hanime untuk melihat command anime")
+        .addField("3. Other Command", "gunakan -other untuk melihat command other");
 
 		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
 }
