73e8b49db41dbd86a240948f5490a5755a53d1f3
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:26:34 +0700

    Update stats.js

--- cmds/stats.js
+++ cmds/stats.js
@@ -21,6 +21,22 @@ setInterval(function() {
     }
 }, 1000)
 module.exports.run = async (bot, message, args) => {
+     if(message.author.id !== "264015893434990592") if(message.author.id !== "438019097683558420") return message.channel.send(":no_entry_sign: You cannot use this command because, you are not a developer.")
+
+  rebootBot(message.channel);
+       function rebootBot(channel) {
+           message.react('✅')
+               .then(message => bot.destroy())
+               .then(message => bot.destroy())
+               .then(() => bot.login(config.token));
+           message.channel.send("``[Q u e l a] has successfully rebooted!``")
+       }
+    }
+exports.help = {
+	name: "reboot"
+}
+
+
     message.channel.send(`= STATISTICS =
 • Bot        :: ${bot.user.tag}
 • Developer  :: Q u e l a#1855
