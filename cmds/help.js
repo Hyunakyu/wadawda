df862450c2a73f93eb6cfcbaa971fd2c650f98dd
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:06:07 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -10,7 +10,8 @@ exports.run = async (bot, message, args) => {
         .addField("1. Command Music", "gunakan -music untuk melihat command music")
 	.addField("2. Command Fun", "gunakan -fun untuk melihat command music")
         .addField("2. Command Anime", "gunakan -hanime untuk melihat command anime")
-        .addField("3. Other Command", "gunakan -other untuk melihat command other");
+        .addField("3. Other Command", "gunakan -other untuk melihat command other")
+	.addField("4. Owner Command", "gunakan -owner untuk melihat command Owner");
 
 		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
 }
