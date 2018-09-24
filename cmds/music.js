ccbb74c8b87e3445ef1206846cc0f664f99cf6a1
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:14:25 +0700

    Update music.js

--- cmds/music.js
+++ cmds/music.js
@@ -6,7 +6,15 @@ exports.run = async (bot, message, args) => {
 		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
         .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
         .setAuthor("Music Command")
-        .addField("1. -play / -p", "Untuk Memutar Musik");
+	.addField("Aktivasi Terlebhi Dahulu BotMUSIC", "Dengan Cara -play, -play blackpink")
+        .addField("1. -play / -p", "Untuk Memutar Musik")
+	.addField("2. -skip", "Untuk Skip Musik Keselanjutnya")
+	.addField("3. -stop", "Untuk Sto[ Musik")
+	.addField("4. -volume", "Untuk Atur Volume Musik")
+	.addField("5. -np", "Untuk Mengetahui Lagu yang lagi play")
+	.addField("6. -queue/ -q", "Melihat list lagu")
+	.addField("7. -pause", "mempause lagu")
+	.addField("8. -resume", "memulai kembali lagu yang dipause");
 
 		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
 }
