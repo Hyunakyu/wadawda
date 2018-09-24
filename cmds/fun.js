a29eec4368228e882ee4a78d7d3246317bb57f41
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:02:05 +0700

    Update fun.js

--- cmds/fun.js
+++ cmds/fun.js
@@ -4,15 +4,16 @@ module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setTitle(":stuck_out_tongue_closed_eyes: Fun")
     .setTimestamp()
-    .addField("-8ball", "Ask the magical 8ball a question.")
-    .addField("-avatar", "Get a member's avatar.")
-    .addField("-doggo", "Get a random dog image.")
-    .addField("-cat", "Get random cat image.")
-    .addField("-meme", "Get a random meme.")
-    .addField("-joke", "Get a random joke.")
-    .addField("-roll", "Roll a number between 1-100")
-    .addField("-poke", "Poke a user.")
-    .addField("-slots", "Roll the slots!");
+    .addField("1. -quiz", "get a question.")
+    .addField("2. -8ball", "Ask the magical 8ball a question.")
+    .addField("3. -avatar", "Get a member's avatar.")
+    .addField("4.-doggo", "Get a random dog image.")
+    .addField("5. -cat", "Get random cat image.")
+    .addField("6. -meme", "Get a random meme.")
+    .addField("7. -joke", "Get a random joke.")
+    .addField("8. -roll", "Roll a number between 1-100")
+    .addField("10. -poke", "Poke a user.")
+    .addField("11. -slots", "Roll the slots!");
     message.channel.send(embed);
 }
 module.exports.help = {
