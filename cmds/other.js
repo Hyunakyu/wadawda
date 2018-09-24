e9a347ca6133fcccdbeeea6f0c0bf4609f9c99f3
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:17:37 +0700

    Update other.js

--- cmds/other.js
+++ cmds/other.js
@@ -6,15 +6,16 @@ exports.run = async (bot, message, args) => {
 		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
         .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
         .setAuthor("Other Command")
-        .addField("1. -ping", "cuma buat test Latency:3")
-        .addField("2. -prefix", "Untuk mengganti prefix")
-        .addField("3. -autorole", "Untuk set autorole")
-        .addField("3. -weather", "untuk melihat cuaca")
-        .addField("4. -xchat", "untuk menghapus history chat")
-        .addField("5. -kick", "Cara -kick@Mention user untuk mengkick, Need Permission Role High")
-        .addField("6. -ban", "Cara -ban@Mention user untuk mengkick, Need Permission Role Admin/Administrator")
-        .addField("7. -serverinfo", "Mengetahui Informasi Server Discord")
-        .addField("8. -botinfo", "Mengetahui Pembuat Bot");
+	.addField("1. -uptime", "Life Time BOT")
+        .addField("2. -ping", "cuma buat test Latency:3")
+        .addField("3. -prefix", "Untuk mengganti prefix")
+        .addField("4. -autorole", "Untuk set autorole")
+        .addField("5. -weather", "untuk melihat cuaca")
+        .addField("6. -xchat", "untuk menghapus history chat")
+        .addField("7. -kick", "Cara -kick@Mention user untuk mengkick, Need Permission Role High")
+        .addField("8. -ban", "Cara -ban@Mention user untuk mengkick, Need Permission Role Admin/Administrator")
+        .addField("9. -serverinfo", "Mengetahui Informasi Server Discord")
+        .addField("10. -botinfo", "Mengetahui Pembuat Bot");
 
 		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
 }
