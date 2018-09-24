839cf875ffe0dcc6750515aeed1ca40228904602
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:05:46 +0700

    Create owner.js

---
+++ cmds/owner.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+		let bicon = bot.user.displayAvatarURL; // untuk menampilkan avatar dari bot kalian
+		let botembed = new Discord.RichEmbed()
+		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
+        .setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
+        .setAuthor("Owner Command")
+		    .addField("Prefix - ","Change Prefix | -prefix")
+        .addField("1. Command Music", "gunakan -music untuk melihat command music")
+	      .addField("2. Command Fun", "gunakan -fun untuk melihat command music")
+        .addField("2. Command Anime", "gunakan -hanime untuk melihat command anime")
+        .addField("3. Other Command", "gunakan -other untuk melihat command other")
+	      .addField("4. Owner Command", "gunakan -other untuk melihat command other");
+
+		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
+}
+
+exports.help = {
+	name: "Owner"
+}
