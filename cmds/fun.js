9184bf07cb1c9f8b8a59a3523fde0445be904795
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-24 22:44:00 +0700

    Update fun.js

--- cmds/fun.js
+++ cmds/fun.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
-    .setTitle(":stuck_out_tongue_closed_eyes: Fun")
+    .setTitle(":robot: Fun")
     .setTimestamp()
     .addField("1. -quiz", "get a question.")
     .addField("2. -8ball", "Ask the magical 8ball a question.")
